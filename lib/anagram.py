b35830920046e64a40c055cfc02981ae4b5f602a
Sharon <Sharonaquilla28@gmail.com>
2024-06-03 14:49:01 +0300

    anagram lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram :
+    def __init__(self , word) -> None:
+        self.word = word
+        self.sorted_word = sorted(word)
+
+    def match(self , possible_annagram):
+        return [anagram for anagram in possible_annagram if sorted(anagram)==self.sorted_word and anagram.lower()!= self.word.lower()]
+    
+listen =Anagram('listen')
+print(listen.match(['enlists','google','inlets']))
+
+        
